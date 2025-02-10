740371c7aa5137b1baac1e99c7cc40997b0805e3
Soar Qin <soarchin@gmail.com>
2025-02-11 00:53:56 +0800

    work in progress

--- src/eldenring/param.c
+++ src/eldenring/param.c
@@ -10,31 +10,69 @@
 
 #include "pointers.h"
 
-#include "param_defs.h"
+#define uthash_malloc(sz) LocalAlloc(0, sz)
+#define uthash_free(ptr,sz) LocalFree(ptr)
+#define uthash_bzero(a,n) ZeroMemory(a, n)
+#define uthash_strlen(s) lstrlenA(s)
 
+#include <uthash.h>
+
+#include <windows.h>
 #include <stdio.h>
 
+typedef struct {
+    uintptr_t vtable;
+    uintptr_t unk0;
+    wstring_impl_t name;
+    uintptr_t unk1[9];
+    struct {
+        uintptr_t unk0[16];
+        param_table_t *data;
+    } *path;
+} param_t;
+
+typedef struct {
+    param_t **start;
+    param_t **end;
+} param_collection_t;
+
+typedef struct {
+    const wchar_t *name;
+    const param_table_t *param;
+
+    UT_hash_handle hh;
+} param_type_t;
+
+static param_type_t *param_types = NULL;
+
 bool param_load_table() {
+    HASH_CLEAR(hh, param_types);
     pointers_init(INIT_CS_REGULATION_MANAGER);
     if (!pointers.cs_regulation_manager) {
         return false;
     }
-    const param_collection_t *param_collection = (const param_collection_t *)(*(uintptr_t*)pointers.cs_regulation_manager + 0x18);
+    const param_collection_t *param_collection = (const param_collection_t*)(*(uintptr_t*)pointers.cs_regulation_manager + 0x18);
     if (!param_collection->start || !param_collection->end) {
         return false;
     }
     fprintf(stderr, "%p %p %zd\n", param_collection->start, param_collection->end, param_collection->end - param_collection->start);
     for (param_t **current = param_collection->start; current < param_collection->end; current++) {
         const param_t *param = *current;
-        const uintptr_t ptr = *(uintptr_t*)(*(uintptr_t*)((const uintptr_t)param + 0x80) + 0x80);
-        uint16_t count = param->path->data->count;
-        fwprintf(stderr, L"%ls %u\n", wstring_impl_str(&param->name), count);
-        const param_entry_offset_t *entries = param->path->data->entries;
-        if (count > 5) count = 5;
-        for (uint16_t i = 0; i < count; i++) {
-            const param_entry_offset_t *entry = entries + i;
-            fwprintf(stderr, L"%llu %zd\n", entry->param_id, entry->offset);
-        }
+        param_type_t *pt = LocalAlloc(0, sizeof(param_type_t));
+        pt->name = wstring_impl_str(&param->name);
+        pt->param = param->path->data;
+        HASH_ADD_KEYPTR(hh, param_types, pt->name, lstrlenW(pt->name) * sizeof(wchar_t), pt);
     }
     return true;
+}
+
+void param_unload() {
+    HASH_CLEAR(hh, param_types);
+}
+
+const param_table_t *param_find_table(const wchar_t *name) {
+    param_type_t *res = NULL;
+    HASH_FIND(hh, param_types, name, lstrlenW(name) * sizeof(wchar_t), res);
+    if (!res) return NULL;
+    return res->param;
 }
--- src/eldenring/param.h
+++ src/eldenring/param.h
@@ -24,26 +24,33 @@ typedef struct {
     uintptr_t vtable;
     uint16_t unk0;
     uint16_t count;
-    uintptr_t unk2[6];
+    uintptr_t unk1[6];
     param_entry_offset_t entries[0];
-} param_data_t;
-
-typedef struct {
-    uintptr_t vtable;
-    uintptr_t unk0;
-    wstring_impl_t name;
-    uintptr_t unk1[9];
-    struct {
-        uintptr_t unk0[16];
-        param_data_t *data;
-    } *path;
-} param_t;
-
-typedef struct {
-    param_t **start;
-    param_t **end;
-} param_collection_t;
+} param_table_t;
 
 #pragma pack(pop)
 
 bool param_load_table();
+void param_unload();
+const param_table_t *param_find_table(const wchar_t *name);
+
+#define param_table_find_id(t, id, tp) { \
+    uint16_t count = (t)->count; \
+    for (uint16_t i = 0; i < count; i++) { \
+        const param_entry_offset_t *entry = &((t)->entries[i]); \
+        if (entry->param_id == id) { \
+            return (tp*)((uintptr_t)t + entry->offset); \
+        } \
+    } \
+}
+
+#define param_table_iterate_begin(t, tp, var) { \
+    uint16_t count = (t)->count; \
+    for (uint16_t i = 0; i < count; i++) { \
+        const param_entry_offset_t *entry = &((t)->entries[i]); \
+        tp *var = (tp*)((uintptr_t)t + entry->offset); \
+        if (!var) { \
+            continue; \
+        }
+
+#define param_table_iterate_end() } }
